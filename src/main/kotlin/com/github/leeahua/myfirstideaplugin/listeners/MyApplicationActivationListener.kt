06d827ee8cf7aa7a89d8eacd342993aabad4a976
GitHub Action <action@github.com>
2023-09-28 06:27:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/leeahua/myfirstideaplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/leeahua/myfirstideaplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.leeahua.myfirstideaplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
